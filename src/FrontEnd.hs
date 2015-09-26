871108dfa38b8db24d7ac0004592687a68e53f2f
John Haynes <john.andrew.haynes@gmail.com>
2015-09-27 01:51:19 +1000

    bidirectional message passing between front and back-end

---
+++ src/FrontEnd.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE EmptyDataDecls, OverloadedStrings #-}
+module Hello where
+
+import Fay.Text
+import FFI
+
+data XMLHttpRequest
+
+type Url = String
+
+ajax :: Url -> (Text -> Fay ()) -> Fay ()
+ajax url f = do
+    req <- xmlHttpRequest    
+    open req url
+    setReadyHandler req $ do
+        done <- isDone req
+        when done $ responseText req >>= f
+    send req ""
+
+    where
+    xmlHttpRequest :: Fay XMLHttpRequest
+    xmlHttpRequest = ffi "new XMLHttpRequest()"
+
+    open :: XMLHttpRequest -> String -> Fay ()
+    open = ffi "%1.open(\"GET\", %2, true)"
+
+    setReadyHandler :: XMLHttpRequest -> Fay () -> Fay ()
+    setReadyHandler = ffi "%1['onreadystatechange'] = %2"
+
+    isDone :: XMLHttpRequest -> Fay Bool
+    isDone = ffi "4 == %1['readyState']"
+    
+    responseText :: XMLHttpRequest -> Fay Text
+    responseText = ffi "%1['responseText']"
+
+    send :: XMLHttpRequest -> String -> Fay ()
+    send = ffi "%1.send(%2)"
+
+
+main :: Fay ()
+main = do
+    
+    ajax "/addUrl/something" print
+    
+    
+    
--- src/Monitor.hs
+++ src/Monitor.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Data.Text.Lazy         as T              (toStrict, unpack)
+import Data.Text.Lazy         as T              (toStrict, pack)
 import Web.Spock.Safe                           
 import Network.Wai.Middleware.Static            (staticPolicy, addBase)
 import Text.Hamlet                              (Html, shamlet)
@@ -10,31 +10,45 @@ import Text.Blaze.Html.Renderer.Text            (renderHtml)
 import Text.Blaze.Internal                      (Markup)
 
 import Communication
-import Data.ByteString.Char8                as C8
-import Control.Monad.IO.Class               (liftIO)
+import Data.ByteString.Char8                    as C8
+import Control.Monad.IO.Class                   (liftIO)
 
 main :: IO ()
 main =
     runSpock 8080 $
         spockT id $ do
+            middleware . staticPolicy . addBase $ "static"
+            spockApp
 
-            get root $ fromTemplate mainPage
+spockApp = do
 
-            get ("addUrl" <//> var) $ \url -> liftIO . addUrl $ url
+    get root $ fromTemplate mainPage
+
+    get ("addUrl" <//> var) $ \url -> do
+        msg <- liftIO . addUrl $ url
+        text . toStrict . T.pack . show $ msg
+
+    get ("queueSize" <//> var) $ \qn -> do
+        msg <- liftIO . queueSize $ qn
+        text . toStrict . T.pack . show $ msg
 
     where
     fromTemplate :: Html -> ActionT IO a
     fromTemplate = html . toStrict . renderHtml
 
-addUrl url = do
-    sendAndGetReply $ CommandMessage (AddUrl (C8.pack url))
+addUrl :: String -> IO Message
+addUrl url = sendAndGetReply $ CommandMessage (AddUrl (C8.pack url))
 
-mainPage :: Markup        
+queueSize :: String -> IO Message
+queueSize name = sendAndGetReply . QuestionMessage . GetQueueSize $ (read name :: QueueName)
+
+mainPage :: Markup
 mainPage = [shamlet|
     <!DOCTYPE html>
     <html lang="en-AU">
         <head>
             <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
             <title>Crawler Monitor
+            <script type="text/javascript" src="fay.js">
         <body>
 |]
