6db993a81c4cc39ba26754c7111f342460db2348
John Haynes <john.andrew.haynes@gmail.com>
2016-03-10 19:52:39 +1100

    WIP first pass writing simple command line arg parser

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,6 @@ module Main where
 import Communication
 import CountedQueue
 import Crawl
-import Data.ByteString.Char8            (ByteString, pack)
 import qualified PoliteQueue    as PQ
 import Settings
 import Types
@@ -16,10 +15,13 @@ import Control.Applicative              ((<$>))
 import Control.Concurrent               (threadDelay)
 import Control.Concurrent.STM           (atomically, newTVarIO, readTVar)
 import Control.Monad                    (unless)
-
+import Data.List
+import Data.Maybe                       (mapMaybe)
+import qualified Data.Map               as Map
+import qualified Data.Set               as Set
 import Network.HTTP.Conduit             (newManager, tlsManagerSettings, mkManagerSettings)
 import Network.Connection               (TLSSettings (TLSSettingsSimple))
-
+import Safe
 import qualified STMContainers.Set as S
 import qualified STMContainers.Map as M
 
@@ -43,6 +45,7 @@ createCrawlerState = do
     urlsFailed <- M.newIO
     return $ CrawlerState formInstructions crawlerStatus urlQueue storeQueue loggingQueue manager cookieList urlPatterns urlsInProgress urlsCompleted urlsFailed
 
+{-
 data StartMode = WithFrontEnd
                | Headless CanonicalUrl ByteString
                | FailStartup String
@@ -52,7 +55,20 @@ parseArgs [strStartUrl, includePattern] =
     case canonicaliseString strStartUrl of
         Nothing -> FailStartup $ "Could not canonicalise url: " ++ strStartUrl
         Just startUrl -> Headless startUrl (pack includePattern)
-parseArgs _ = WithFrontEnd
+        parseArgs _ = WithFrontEnd -}
+
+flags = Set.fromList ["-i", "-u"]
+
+parseArgs args = do
+
+    print       . Map.unions
+                . mapMaybe (\gr -> Map.singleton <$> headMay gr <*> tailMay gr)
+                . filter (\gr -> maybe False (`Set.member` flags) (headMay gr))
+                . groupBy (\a b -> isFlag a && not (isFlag b))
+                $ args
+
+    where
+    isFlag x = length x > 1 && head x == '-'
 
 main :: IO ()
 main = do
@@ -63,8 +79,14 @@ main = do
 
     initialiseWorkers crawlerState
 
-    startMode <- parseArgs <$> getArgs
+    --startMode <- parseArgs <$> getArgs
 
+    args <- getArgs
+    
+    parseArgs args
+    
+    
+    {-
     case startMode of
         FailStartup msg -> error msg
         WithFrontEnd -> run (getCrawlerStatus crawlerState)
@@ -80,4 +102,4 @@ main = do
         halted <- (== Halted) <$> (atomically . readTVar $ crawlerStatus)
         unless halted $ do
             threadDelay 1000000
-            run crawlerStatus
+            run crawlerStatus -}
