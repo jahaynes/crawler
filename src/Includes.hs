8da06ca9ff43c509be9457486ba4e29c69d3517a
John Haynes <john.andrew.haynes@gmail.com>
2016-03-10 21:09:04 +1100

    abort on no include patterns

--- src/Includes.hs
+++ src/Includes.hs
@@ -4,20 +4,23 @@ module Includes where
 
 import Types
 
-import Control.Applicative          ((<$>))
-import Control.Monad.STM            (STM)
-import qualified Data.ByteString.Char8 as C8
-
-import qualified Data.Map   as M
+import Control.Applicative                      ((<$>))
+import Control.Monad.STM                        (STM, atomically)
+import qualified Data.ByteString.Char8  as C8
+import qualified STMContainers.Set      as S
+import qualified Data.Map               as M
 
 checkAgainstIncludePatterns :: CrawlerState -> CanonicalUrl -> STM Bool
 checkAgainstIncludePatterns crawlerState (CanonicalUrl url) =
     any (`C8.isInfixOf` url) <$> setAsList (getUrlPatterns crawlerState)
 
 initialiseIncludes :: CrawlerState -> OptionMap -> IO ()
-initialiseIncludes crawlerState (OptionMap optionMap) = do
+initialiseIncludes crawlerState (OptionMap optionMap) =
 
     case M.lookup (OptionFlag "-i") optionMap of
-        Nothing -> C8.putStrLn "Warning, no include patterns included"
-        Just includePatterns -> do
-            print includePatterns
+        Nothing -> error "Fatal, no include patterns included!"
+        Just includePatterns ->
+            mapM_ (\i -> do
+                let pattern = C8.pack i
+                atomically . S.insert pattern . getUrlPatterns $ crawlerState
+                C8.putStrLn $ C8.concat ["Added pattern: ", pattern]) includePatterns
