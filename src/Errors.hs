d1bd9e73e16294b4482821b2f3504e21e0adac9a
John Haynes <john.andrew.haynes@gmail.com>
2016-05-21 23:24:47 +1000

    remove unused

--- src/Errors.hs
+++ src/Errors.hs
@@ -2,16 +2,12 @@
 
 module Errors where
 
+import CountedQueue                     (sourceQueue)
 import Types
 
 import Control.Monad.IO.Class           (liftIO)
 import Control.Monad.Trans.Resource     (runResourceT)
-import CountedQueue                     (sourceQueue)
-import Data.ByteString.Char8 as C8      (ByteString, concat)
-
 import Data.Conduit
-import Data.Conduit.List as CL
-import Data.Conduit.Binary
 
 logErrors :: CrawlerState -> LogFunction -> IO ()
 logErrors crawlerState logFunction =
