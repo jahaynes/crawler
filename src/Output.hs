e21fac3420f754c5dc9b8a095263f2aaf75615ea
John Haynes <john.andrew.haynes@gmail.com>
2016-05-21 23:03:26 +1000

    removed unused

--- src/Output.hs
+++ src/Output.hs
@@ -4,13 +4,9 @@ module Output where
 
 import CountedQueue                     (sourceQueue)
 import Types
-import Control.Concurrent               (ThreadId)
 import Control.Monad.IO.Class           (liftIO)
 import Control.Monad.Trans.Resource     (runResourceT)
-import Data.ByteString.Char8 as BS      (ByteString, pack, intercalate, concat)
 import Data.Conduit
-import Data.Conduit.Binary
-import qualified Data.Conduit.List as CL
 
 storePages :: CrawlerState -> (CrawledDocument -> IO a) -> IO ()
 storePages crawlerState storeFunc = do
@@ -19,8 +15,3 @@ storePages crawlerState storeFunc = do
         $$ awaitForever $ liftIO . storeFunc
 
     return ()
-
-    where
-    redirectsLine :: (ThreadId, [ByteString]) -> ByteString
-    redirectsLine (tid, redirects) = BS.concat [pack . show $ tid, " finished\t", intercalate " <- " redirects, "\n"]
-
