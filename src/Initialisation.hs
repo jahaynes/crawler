07bc2a55e67cd0658619206eaf91e81df9dde53c
John Haynes <john.andrew.haynes@gmail.com>
2016-07-24 15:08:01 +1000

    moved form instructions and proxy initialisation into initialisation

--- src/Initialisation.hs
+++ src/Initialisation.hs
@@ -2,20 +2,26 @@
 
 module Initialisation where
 
-import CountedQueue                             (writeQueue)
-import Crawl
-import Settings
-import Shared
-import Types
-import Urls
+import           CountedQueue           (writeQueue)
+import           Crawl
+import           Shared
+import           Types
+import           Urls
 
-import Control.Monad.STM                        (atomically)
+import           Control.Concurrent.STM (atomically, writeTVar)
 import qualified Data.ByteString.Char8  as C8
-import Data.List
-import Data.Maybe                       (mapMaybe)
-import Safe
+import           Data.List              (groupBy, partition, union)
+import           Data.List.Split        (splitOn)
 import qualified Data.Map               as M
+import           Data.Maybe             (mapMaybe)
+import           Network.URI            (unEscapeString)
+import           Safe
 import qualified STMContainers.Set      as S
+import           Text.Read              (readMaybe)
+
+newtype OptionFlag = OptionFlag String deriving (Show, Eq, Ord)
+
+newtype OptionMap = OptionMap (M.Map OptionFlag [String]) deriving Show
 
 optionMapFromArgs :: [String] -> OptionMap
 optionMapFromArgs =   OptionMap
@@ -74,7 +80,7 @@ initialiseSettings crawler args = do
                 insertStartUrls . mapMaybe (canonicaliseByteString . trim)
                                 . filter (not . C8.null)
                                 . concatMap C8.lines
-                                $ files        
+                                $ files
 
         where
         insertStartUrls =
@@ -83,4 +89,63 @@ initialiseSettings crawler args = do
                 case result of
                     Success -> C8.putStrLn $ C8.concat ["Added Url: ", u]
                     Failure reason -> atomically $ writeQueue (getLogQueue crawler) (LoggableError cu (C8.concat ["Could not add Url: ", u, "\n", "Reason: ", reason])))
+
+    initialiseProxy :: CrawlerSettings -> OptionMap -> IO ()
+    initialiseProxy crawlerSettings (OptionMap optionMap) =
+
+            case parseProxy of
+                Just proxySettings -> atomically $ writeTVar (getProxySettings crawlerSettings) (Just proxySettings)
+                Nothing -> return ()
+
+            where
+            parseProxy :: Maybe ProxySettings
+            parseProxy = do
+                proxy <- headMay =<< M.lookup (OptionFlag "-p") optionMap
+                case splitOn ":" proxy of
+                    [address,strPort] -> do
+                        port <- readMaybe strPort
+                        return $ ProxySettings (C8.pack address) port
+                    _ -> Nothing
+
+    initialiseFormInstructions :: CrawlerSettings -> OptionMap -> IO ()
+    initialiseFormInstructions crawlerSettings (OptionMap optionMap) =
+
+        case M.lookup (OptionFlag "-ff") optionMap of
+            Nothing -> return ()
+            Just formFiles -> do
+                formFileContents <- mapM readFile formFiles
+                let processed = map processFormInstructions formFileContents
+                    formInstructions = SuppliedFormActions $ M.unions processed
+                atomically $ writeTVar (getFormInstructions crawlerSettings) formInstructions
+                putStrLn $ "Inserted Form instructions: \n" ++ show formInstructions
+
+        where
+        processFormInstructions :: String -> M.Map Label (UrlRegex, FormActionRegex, FormParameters)
+        processFormInstructions formFile = do
+            let ls = filter (not . null) . splitOn [""] . lines $ formFile
+                instructions = mapMaybe chunkToInstruction ls
+            M.fromList instructions
+
+            where
+            chunkToInstruction :: [String] -> Maybe (Label, (UrlRegex, FormActionRegex, FormParameters))
+            chunkToInstruction chunk = do
+
+                let keysAndVals = map (splitOn "=") chunk
+
+                    tuples = map (\[a,b] -> (a,b))
+                        . filter (\x -> length x == 2) $ keysAndVals
+
+                    (required, paramStrings) =
+                        partition (\x -> fst x `elem` ["Label", "UrlRegex", "FormActionRegex"]) tuples
+
+                label <- getVal "Label" required
+                urlRegex <- getVal "UrlRegex" required
+                formActionRegex <- getVal "FormActionRegex" required
+                let params = map (both (C8.pack . unEscapeString)) paramStrings
+
+                return (Label label, (UrlRegex urlRegex, FormActionRegex formActionRegex, FormParameters params))
+
+                where
+                getVal :: String -> [(String, String)] -> Maybe C8.ByteString
+                getVal key = headMay . map (C8.pack . snd) . filter (\x -> fst x == key)
 
--- src/Settings.hs
+++ src/Settings.hs
@@ -2,24 +2,8 @@
 
 module Settings where
 
-import Shared (both)
-import Types
-
-import Control.Concurrent.STM        (writeTVar, atomically)
-import qualified Data.ByteString.Char8   as C8
-import Data.List                    (partition)
-import Data.List.Split              (splitOn)
-import Data.Maybe                   (mapMaybe)
-import qualified Data.Map as M
-import Network.HTTP.Conduit         (Request, Cookie, applyBasicAuth)
-import Network.HTTP.Types.Header    (RequestHeaders, hUserAgent)
-import Network.URI                  (unEscapeString)
-import Safe
-import Text.Read                    (readMaybe)
-
-newtype OptionFlag = OptionFlag String deriving (Show, Eq, Ord)
-
-newtype OptionMap = OptionMap (M.Map OptionFlag [String]) deriving Show
+import           Network.HTTP.Conduit      (Cookie, Request, applyBasicAuth)
+import           Network.HTTP.Types.Header (RequestHeaders, hUserAgent)
 
 numStartCrawlers :: Int
 numStartCrawlers = 20
@@ -49,61 +33,3 @@ stepMode = False
 customHeaders :: RequestHeaders
 customHeaders = [(hUserAgent, "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Crawler/0.1")]
 
-initialiseProxy :: CrawlerSettings -> OptionMap -> IO ()
-initialiseProxy crawlerSettings (OptionMap optionMap) = 
-
-    case parseProxy of
-        Just proxySettings -> atomically $ writeTVar (getProxySettings crawlerSettings) (Just proxySettings)
-        Nothing -> return ()
-
-    where
-    parseProxy :: Maybe ProxySettings
-    parseProxy = do
-        proxy <- headMay =<< M.lookup (OptionFlag "-p") optionMap
-        case splitOn ":" proxy of
-            [address,strPort] -> do
-                port <- readMaybe strPort
-                return $ ProxySettings (C8.pack address) port
-            _ -> Nothing
-
-initialiseFormInstructions :: CrawlerSettings -> OptionMap -> IO ()
-initialiseFormInstructions crawlerSettings (OptionMap optionMap) =
-
-    case M.lookup (OptionFlag "-ff") optionMap of
-        Nothing -> return ()
-        Just formFiles -> do
-            formFileContents <- mapM readFile formFiles
-            let processed = map processFormInstructions formFileContents
-                formInstructions = SuppliedFormActions $ M.unions processed
-            atomically $ writeTVar (getFormInstructions crawlerSettings) formInstructions
-            putStrLn $ "Inserted Form instructions: \n" ++ show formInstructions 
-
-    where
-    processFormInstructions :: String -> M.Map Label (UrlRegex, FormActionRegex, FormParameters)
-    processFormInstructions formFile = do
-        let ls = filter (not . null) . splitOn [""] . lines $ formFile
-            instructions = mapMaybe chunkToInstruction ls
-        M.fromList instructions
-
-        where
-        chunkToInstruction :: [String] -> Maybe (Label, (UrlRegex, FormActionRegex, FormParameters))
-        chunkToInstruction chunk = do
-
-            let keysAndVals = map (splitOn "=") chunk
-
-                tuples = map (\[a,b] -> (a,b))
-                    . filter (\x -> length x == 2) $ keysAndVals
-
-                (required, paramStrings) =
-                    partition (\x -> fst x `elem` ["Label", "UrlRegex", "FormActionRegex"]) tuples
-
-            label <- getVal "Label" required
-            urlRegex <- getVal "UrlRegex" required
-            formActionRegex <- getVal "FormActionRegex" required
-            let params = map (both (C8.pack . unEscapeString)) paramStrings
-
-            return (Label label, (UrlRegex urlRegex, FormActionRegex formActionRegex, FormParameters params))
-
-            where
-            getVal :: String -> [(String, String)] -> Maybe C8.ByteString
-            getVal key = headMay . map (C8.pack . snd) . filter (\x -> fst x == key)
